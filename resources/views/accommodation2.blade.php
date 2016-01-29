5d9d10a685fe82aaec667da32d7d7aed05c70aee
chamalsilva <chamalsilva321@gmail.com>
2016-01-29 09:33:24 +0530

    testing 1 2 3

--- resources/views/accommodation2.blade.php
+++ resources/views/accommodation2.blade.php
@@ -284,7 +284,7 @@
     	<!-- Footer Links | START -->
     	<div id="footerlinks">
         	<div class="centre">
-            	            	<span>Copyright &copy; <script>var d = new Date(); document.write(d.getFullYear());</script> <strong>Amalya Reach Holiday Resort</strong></span><a href="index.html">Home</a><a href="sitemap.html">Sitemap</a><span>
+            	            	<span>Copyright &copy; <script>var d = new Date(); document.write(d.getFullYear());</script> <strong>Amalya Reach Holiday Resort</strong></span><a href="index.html">Home</a><a href="sitemap.html">Sitemap</a></span>
             </div>
         </div>
         <!-- Footer Links | END -->
